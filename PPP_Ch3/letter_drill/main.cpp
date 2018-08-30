a09bbd4d109406a19336fcfa8c15946e13f07791
mbmcc <m.mccourry@gmail.com>
2018-08-30 08:00:19 -0700

    updated the letter program through checkpoint 4

--- PPP_Ch3/letter_drill/main.cpp
+++ PPP_Ch3/letter_drill/main.cpp
@@ -8,22 +8,24 @@ int main(){
   string friend_name;
   cin >> friend_name;
   cout << "Enter m is that person is male, and f if that person is female: \n";
-  char friend_sex {0};
+  char friend_sex =0;
   cin >> friend_sex;
-  string sex = "";
-  cout << "Dear " << first_name <<",\n";
-  cout << "\tHow are you? I am fine. I miss you.\n"  
-  << "How is your business going? Hiked any good mountains lately?\n"
-  << "I would really like to see you. When can we meet?\n"
-  << "Have you seen "<< friend_name << " lately? \n"
-  ;
-  if(friend_sex=='m') 
-    sex = "him";
-  else if (friend_sex=='f')
-    sex = "her";
-  cout << "If you see " << friend_name << " please ask "<< sex <<" to call me.\n";
+  
+  cout << "Dear " << first_name <<",\n"
+   << "\tHow are you? I am fine. I miss you.\n"
+   << "How is your business going? Hiked any good mountains lately? \n"
+   << "I would really like to see you. When can we meet?\n"
+   << "Have you seen "<< friend_name << " lately? ";
+  if (friend_sex == 'm')
+    cout << "If you see " <<friend_name<< " please ask him to call me. \n";
+  else if (friend_sex == 'f')
+    cout << "If you see " <<friend_name<< " please ask her to call me. \n";
+  cout << "Sincerely, \n\n Matt \n" << endl;
 
 
+
+
+ 
     
 
 }
